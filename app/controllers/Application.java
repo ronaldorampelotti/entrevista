710e54c0755ae280766d2fadb0ce8ee62aa97ec4
Ronaldo Rampelotti <ronaldo.rampelotti@gmail.com>
2016-01-21 15:30:52 -0200

    Estrutura inicial

---
+++ app/controllers/Application.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import models.Pessoa;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class Application extends Controller {
+
+    public Result index() {
+    	
+    	return ok();
+    }
+    
+    public Result pessoas() {
+    	
+    	ArrayList<Pessoa> listaPessoas = new ArrayList<>();
+    	
+    	for(int i=0; i<10; i++) {
+    		Pessoa pesAtual = new Pessoa();
+    		
+    		pesAtual.setNome("Robo " + i);
+    		pesAtual.setSobrenome("Alias " + Math.random() );
+    		pesAtual.setDataNascimento(new Date());
+    		
+    		listaPessoas.add(pesAtual);
+    	}
+    	return ok(Json.toJson(listaPessoas));
+    }
+
+}
---
+++ app/models/Pessoa.java
@@ -0,0 +1,40 @@
+package models;
+
+import java.util.Date;
+
+public class Pessoa {
+	
+	private String nome;
+	
+	private String sobrenome;
+	
+	private Date dataNascimento;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	
+	
+
+}
